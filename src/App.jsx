cfe568ac6c36227a3517a1a4e8666dd905f55c8e
Tasdeed <ayonaziz@gmail.com>
2024-02-05 17:44:59 -0500

    Ex 2.6completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,37 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  console.log(persons);
   const [newName, setNewName] = useState("");
+  //console.log(newName);
 
-  const handleChange = (event) => {
-    console.log(event.target.value);
+  const addContact = (e) => {
+    e.preventDefault();
+    console.log("button initiated", e.target);
+    setPersons([...persons, { name: newName }]);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addContact}>
+        <div>debug: {newName}</div>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((ele, index) => (
+        <p key={index}>{ele.name}</p>
+      ))}
     </div>
   );
 };
