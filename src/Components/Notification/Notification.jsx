c4c77d45fe7157a2aeec639cead01bc72d3c9e38
Tasdeed <ayonaziz@gmail.com>
2024-02-25 02:21:47 -0500

    tailwind styling

--- src/Components/Notification/Notification.jsx
+++ src/Components/Notification/Notification.jsx
@@ -1,9 +1,17 @@
 const Notification = ({ message }) => {
-  return <div className="notification">{message}</div>;
-};
+  return (
+    <div className='bg-slate-50 text-green-500 text-2xl border-2 border-gray-400 rounded p-4 mb-4 '>
+      {message}
+    </div>
+  )
+}
 
 const ErrorMessage = ({ errorMessage }) => {
-  return <div className="error">{errorMessage}</div>;
-};
+  return (
+    <div className='bg-red-300  text-red-500 text-2xl bore-2 border-gray rounded  p-4 mb-4'>
+      {errorMessage}
+    </div>
+  )
+}
 
-export { Notification, ErrorMessage };
+export { Notification, ErrorMessage }
