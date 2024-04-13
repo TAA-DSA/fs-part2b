96c1c91861243eeef393f3cfea8d88d0ebe6429c
Tasdeed <ayonaziz@gmail.com>
2024-04-13 01:54:42 -0400

    updated login placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -189,7 +189,7 @@ const App = () => {
 
   return (
     <div className='absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]'>
-      <Login liginSubmit={loginBtn} />
+      <Login loginSubmit={loginBtn} />
       <h2 className='flex justify-center py-5  text-4xl font-italic tracking-tight text-green-900 sm:text-6xl'>
         Phonebook
       </h2>
