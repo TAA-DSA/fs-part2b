b7f8aca4755829d583e2e357a4dfeab1765233d5
Tasdeed <ayonaziz@gmail.com>
2025-07-14 15:19:12 -0400

    updated forms

--- src/Components/Notification/Notification.jsx
+++ src/Components/Notification/Notification.jsx
@@ -1,6 +1,6 @@
 const Notification = ({ message }) => {
   return (
-    <div className='bg-slate-50 text-green-500 text-2xl border-2 border-gray-400 rounded p-4 mb-4 '>
+    <div className='bg-slate-50 text-green-500 text-2xl border-2 border-gray-400 rounded p-4 mb-4'>
       {message}
     </div>
   )
